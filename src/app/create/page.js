3d327c18eaa0021a5c524c94e591ae5f2338f7e1
cbk-gemmy <cbk@gemmy.studio>
2025-11-06 00:17:08 +0900

    fix: 라우팅 후 리프레쉬 안됨 수정

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -24,6 +24,7 @@ export default function Create() {
             console.log(result);
             const lastid = result.id;
             router.push(`/read/${lastid}`);
+            router.refresh();
           });
       }}
     >
