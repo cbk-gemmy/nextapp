8e8576d911eda36d895332bbe3d16a0858c6d05a
cbk-gemmy <cbk@gemmy.studio>
2025-11-06 13:30:37 +0900

    feat: 14강 버튼 client component 로 분리

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Link from "next/link";
+import { Control } from "./Control";
 
 export const metadata = {
   title: "Web tutorials",
@@ -27,17 +28,7 @@ export default async function RootLayout({ children }) {
           })}
         </ol>
         {children}
-        <ul>
-          <li>
-            <Link href="/create">Create</Link>
-          </li>
-          <li>
-            <Link href="/update/1">Update</Link>
-          </li>
-          <li>
-            <input type="button" value="delete"></input>
-          </li>
-        </ul>
+        <Control />
       </body>
     </html>
   );
